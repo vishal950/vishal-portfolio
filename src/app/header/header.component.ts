a7911c7c4c92f400fd89ffc9880d3cde93bfe22b
Vishal Sharma <vishal@orderout.com>
2025-08-01 15:30:43 +0530

    menu fix

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,5 @@
 import { NgIf } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -9,5 +9,30 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  menuOpen = false;
+  isMobile = false;
 
+  ngOnInit(): void {
+    this.updateMobileState();
+  }
+
+  toggleMenu(event: Event): void {
+    event.preventDefault();
+    this.menuOpen = !this.menuOpen;
+  }
+
+  onNavClick(): void {
+    if (this.isMobile) {
+      this.menuOpen = false;
+    }
+  }
+
+  @HostListener('window:resize', [])
+  onResize() {
+    this.updateMobileState();
+  }
+
+  private updateMobileState(): void {
+    this.isMobile = window.innerWidth <= 768;
+  }
 }
